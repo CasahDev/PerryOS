dba1159dabfa806baee98c37032d0d0f746a761c
sacha <sacha.laurent71@gmail.com>
2025-12-08 20:42:50 +0100

    feat: Added a struct representing an entry of the IDT table

---
+++ perry_os/include/kernel/idt.h
@@ -0,0 +1,16 @@
+#ifndef _KERNEL_IDT_H
+#define _KERNEL_IDT_H
+
+#include <stdint.h>
+
+// Represents an entry for the IDT table
+struct idt_entry_t {
+   uint16_t offset_low;   // Offset bits 0..15
+   uint16_t selector;     // Selector
+   uint8_t zero;         // Reserved / Zero
+   uint8_t type_attr;    // Type & Attributes
+   uint16_t offset_high;  // Offset bits 16..31
+} __attribute__((packed));
+
+
+#endif
